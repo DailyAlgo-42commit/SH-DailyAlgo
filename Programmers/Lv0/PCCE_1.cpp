670272cf0d459ace52ac0e1636c60a170cce0004
SeoHoSu <gs01llake@gmail.com>
2025-08-18 18:37:56 +0900

    add dummy file for file structure

---
+++ Programmers/Lv0/PCCE_1.cpp
@@ -0,0 +1,26 @@
+/*
+
+문제 설명
+주어진 코드는 변수에 데이터를 저장하고 출력하는 코드입니다. 아래와 같이 출력되도록 빈칸을 채워 코드를 완성해 주세요.
+
+출력 예시
+3
+2
+1
+Let's go!
+
+*/
+
+// 풀이
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    string message = "Let's go!";
+
+    cout << "3" << endl << "2" << endl << "1" << endl;
+    cout << message << endl;
+
+    return 0;
+}
